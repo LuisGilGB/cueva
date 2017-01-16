c6ff29c3c21481ba53a73e1cb14374d939634a7c
Luis <luis.gilgb@gmail.com>
2017-01-17 00:52:11 +0100

    Nuevo intento dbUrl
    var dbUrl = process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME;

--- app.js
+++ app.js
@@ -12,15 +12,7 @@ var cuevaRouter = require('./routes/cuevaRouter');
 var app = express();
 
 var mongoose = require('mongoose');
-var dbUrl = 'mongodb://127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME;;
-// if OPENSHIFT env variables are present, use the available connection info:
-if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    dbUrl = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-		process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    		process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-		process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-		process.env.OPENSHIFT_APP_NAME;
-}
+var dbUrl = process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME;
 mongoose.connect(dbUrl);
 
 var db = mongoose.connection;
