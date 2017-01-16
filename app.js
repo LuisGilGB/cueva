066085b67f675e8d1ff5da6b2b4841ef96106aa4
Luis <luis.gilgb@gmail.com>
2017-01-17 00:39:55 +0100

    Autenticación de la base de datos
    Probemos a meter las credenciales de usuario para conectar con la base de datos.

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ var mongoose = require('mongoose');
 var dbUrl = 'mongodb://127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME;;
 // if OPENSHIFT env variables are present, use the available connection info:
 if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    dbUrl = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
+    dbUrl = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+		process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    		process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+		process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+		process.env.OPENSHIFT_APP_NAME;
 }
 mongoose.connect(dbUrl);
 
